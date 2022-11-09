75c0bc8fa750144e562e22fa64817d54d006aa3e
Andrey Eferemov <samo.dur.andre@gmail.com>
2022-11-09 07:23:57 +0300

    add soulition

---
+++ 3.js
@@ -0,0 +1,8 @@
+/*
+var should not exist in the code.
+Waiting:catName should be the string Oliver.
+Waiting:catSound should be the string Meow!
+*/
+
+let catName = "Oliver";
+let catSound = "Meow!";
