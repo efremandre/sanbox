f90276123f095a8705d3a22728836b9f7928566a
Andrey Eferemov <samo.dur.andre@gmail.com>
2022-11-11 10:33:03 +0300

    add

---
+++ 4.js
@@ -0,0 +1,12 @@
+let myVar = 87;
+myVar++;
+
+let myVar2 = 11;
+myVar2--;
+
+const ourDecimal = 5.7;
+const myDecimal = 2.5;
+
+const product = 2.0 * 2.5;
+const quotient = 4.4 / 2.0; // Change this line
+const remainder = 11 % 3;
