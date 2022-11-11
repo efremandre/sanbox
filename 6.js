51ced6c86e68f91c0c6bc898df6f66e9e6f5846f
Andrey Eferemov <samo.dur.andre@gmail.com>
2022-11-12 02:09:51 +0300

    add

--- 6.js
+++ 6.js
@@ -1,3 +1,5 @@
 const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
 
-const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+const myStr2 = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+const myStr3 = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
