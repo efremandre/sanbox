8cc37bf9a4e43d4976692861178ee42d02a8e8fc
Andrey Eferemov <samo.dur.andre@gmail.com>
2022-11-20 06:52:39 +0300

    edit file 1.js

--- 1.js
+++ 1.js
@@ -1,9 +1,9 @@
 // Initialize the three variables a, b, and c with 5, 10, and "I am a" respectively so that they will not be undefined.
 
 // Only change code below this line
-var a = 5;
-var b = 10;
-var c = "I am a";
+let a = 5;
+let b = 10;
+let c = "I am a";
 // Only change code above this line
 
 a = a + 1;
