98f00add5c15cf0816b449923bf075866ca8b49b
Andrey Eferemov <samo.dur.andre@gmail.com>
2022-11-24 02:47:22 +0300

    add solution

---
+++ 8.js
@@ -0,0 +1,6 @@
+// myName should be set to a string at least 3 characters long.
+// Waiting:You should use two + operators to build myStr with myName inside it.
+
+const myName = "Andrew";
+const myStr = "My name is " + myName + "I am well!";
+
