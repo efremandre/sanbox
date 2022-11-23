d925c5fbd3bf940c0ddd3813811e4d3004914ff8
Andrey Eferemov <samo.dur.andre@gmail.com>
2022-11-24 00:17:24 +0300

    add solution

---
+++ 7.js
@@ -0,0 +1,7 @@
+// myStr should have a single space character between the two strings.
+// Waiting:myStr should have a value of the string This is the first sentence. This is the second sentence.
+// Waiting:You should use the += operator to build myStr.
+
+let myStr = "This is the first sentence. ";
+myStr += "This is the second sentence.";
+
