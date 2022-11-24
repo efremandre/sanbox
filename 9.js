8806a6e278ce96a5f1ab20d48f2e268dab634b27
Andrey Eferemov <samo.dur.andre@gmail.com>
2022-11-24 03:31:20 +0300

    add solution

---
+++ 9.js
@@ -0,0 +1,7 @@
+// someAdjective should be set to a string at least 3 characters long.
+// Waiting:You should append someAdjective to myStr using the += operator.
+
+const someAdjective = "funny";
+let myStr = "Learning to code is ";
+
+myStr += someAdjective;
