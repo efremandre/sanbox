d081efe9440e3ad9117ab54bc8e7385abd051e46
Andrey Eferemov <samo.dur.andre@gmail.com>
2022-11-24 03:33:13 +0300

    add solution

---
+++ 10.js
@@ -0,0 +1,9 @@
+// You should not change the variable declarations in the // Setup section.
+// Waiting:lastNameLength should be equal to eight.
+// Waiting:You should be getting the length of lastName by using .length like this: lastName.length.
+
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
