eebd4f6d0b02c6443771a82729a8390651f691cf
Andrey Eferemov <samo.dur.andre@gmail.com>
2022-11-24 08:59:45 +0300

    add solution cw11

---
+++ cw10.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/578aa45ee9fd15ff4600090d/train/javascript
+
+function sortArray(array) {
+	const even = [];
+	const odd = [];
+
+	array.map(elem => (elem % 2 !== 0) ? even.push(elem) : odd.push(elem));
+	even.sort((a, b) => a - b);
+
+	const result = array.map(elem => (elem % 2 !== 0) ? even.shift() : odd.shift());
+
+	return result;
+}
