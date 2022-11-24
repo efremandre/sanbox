3e1cafbb25dad0ad1bf9c9bbbe18cc4f6f8b066d
Andrey Eferemov <samo.dur.andre@gmail.com>
2022-11-24 03:35:54 +0300

    add solution

---
+++ 11.js
@@ -0,0 +1,8 @@
+// The firstLetterOfLastName variable should have the value of L.
+// Waiting:You should use bracket notation.
+
+let firstLetterOfLastName = " ";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
