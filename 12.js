43371d72b1d10c2def82c71510abe0e06ccef24d
Andrey Eferemov <samo.dur.andre@gmail.com>
2022-11-24 03:39:35 +0300

    add solution

---
+++ 12.js
@@ -0,0 +1,8 @@
+// Waiting:myStr should have a value of the string Hello World.
+// Waiting:You should not change the code above the specified comment.
+
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
